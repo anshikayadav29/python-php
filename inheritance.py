a7de62c6de4a9a4d4a04011e14c97bd51ff327ec
anshikayadav11 <148558969+anshikayadav11@users.noreply.github.com>
2025-05-14 14:35:47 +0530

    Added inheritance.py file

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Ws:
+    def displayinfo(self):
+        print("Welcome to wscube")
+
+class IIP(Ws):
+    def displayinfo(self):
+        super().displayinfo()
+        print("Welcome to IIP")
+
+obj = IIP()
+obj.displayinfo()
