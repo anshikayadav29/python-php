464adfe48fadec754799fd478052f94eb494c65b
anshikayadav11 <148558969+anshikayadav11@users.noreply.github.com>
2025-05-14 10:46:30 +0530

    First commit

--- git.py
+++ git.py
@@ -9,3 +9,20 @@ dec = 34
 
 convertToBinary(dec)
 print()
+
+
+
+
+year =2000
+if (year %400==0)and(year % 100 == 0):
+    print("{0} is a leap year".format(year))
+elif(year % 4 ==0) and (year % 100 !=0):
+    print("{0} is a leap year".format(year))
+
+else:
+    print("{0} is a leap year ".format(year))
+
+
+
+
+
