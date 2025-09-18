3145102f0065127dd435eec46772f150c85e460a
anshikayadav29 <anshikayadav2917@gmail.com>
2025-09-18 12:44:21 +0530

    Commit

---
+++ Solversoduku .py
@@ -0,0 +1,69 @@
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def valid(board, num, pos):
+    row, col = pos
+    # Check row
+    for j in range(len(board[0])):
+        if board[row][j] == num and col != j:
+            return False
+    # Check column
+    for i in range(len(board)):
+        if board[i][col] == num and row != i:
+            return False
+    # Check 3x3 box
+    box_x = col // 3
+    box_y = row // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    row, col = find
+    for num in range(1, 10):
+        if valid(board, num, (row, col)):
+            board[row][col] = num
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+# Example Sudoku (0 = empty cell)
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+print("Sudoku Before Solving:\n")
+print_board(board)
+solve(board)
+print("\nSudoku After Solving:\n")
+print_board(board)
